a97f5c7f9b16949f7fff73ce28a7bde84e8d2e52
CarolFLima <carolflima0@gmail.com>
2022-01-31 20:59:38 -0300

    writing the word to a file

--- bot.py
+++ bot.py
@@ -3,20 +3,25 @@ from email.message import Message
 from json.tool import main
 from random import randint
 from tkinter.tix import MAIN
+from datetime import datetime, timedelta
  
 from telegram import Bot, Update
 from telegram.ext import (CallbackContext, CommandHandler, Dispatcher, Filters,
                           MessageHandler, Updater)
 
-random_word = ''
 
 def start(update:Update, context:CallbackContext):
     random_word = choose_word()
-    print(random_word)
+    f = open("setup.txt", "r+")
+    f.truncate(0)
+    f.write(random_word)
+    f.close()
     update.message.reply_text('_ _ _ _ _')
 
 
 def verify_answer(update: Update, context: CallbackContext):
+    f = open("setup.txt", "r")
+    random_word = f.readline()
     print("palavra é: {}".format(random_word))
     update.message.reply_text('blablabla')
 
